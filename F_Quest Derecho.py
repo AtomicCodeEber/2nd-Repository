daa2810d50d0560c1325907ec88509b07f468648
AtomicCodeEber <145419360+AtomicCodeEber@users.noreply.github.com>
2023-11-26 21:47:05 -0600

    Update F_Quest Derecho.py

--- F_Quest Derecho.py
+++ F_Quest Derecho.py
@@ -295,6 +295,7 @@ if emf == "a":
 else:
     print("Incorrecto :(")
     r18=0
+print("\n")
 
 respuestas = [r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18]
 puntaje_total = sum(respuestas)
